156f7b06c498a54dacb4fe063f423c90a23b4a5b
Celia Martin Herranz <celiamartinherranz11@gmail.com>
2022-11-13 13:26:24 +0100

    search by name and show card function

--- js/main.js
+++ js/main.js
@@ -5,8 +5,10 @@ const inputSearchHtml = document.querySelector('.js-input');
 const buttonSearchHtml = document.querySelector('.js-button-search');
 const charactersHtml = document.querySelector('.js-character-list');
 
+
 //ARRAY
 let characterList = [];
+
 
 //LLAMAR API. 
 fetch('https://breakingbadapi.com/api/characters')
@@ -15,23 +17,44 @@ fetch('https://breakingbadapi.com/api/characters')
     })
     //INFO QUE DEVUELVE LA API
     .then(function(data) { 
-        //LIMPIAR EL CONTENIDO (MENSAJE 'CARGANDO...')
-        charactersHtml.innerHTML = '';
-        //GUARDA RESPUESTA DE API EN NUESTRO ARRAY (LISTA DE PERSONAJES)
-        characterList = data;
-        console.log(characterList);
-        //RECORRE LISTA DE PERSONAJES
-        for (const character of characterList) {
-            console.log(character);
-            //CREAR TARJETA CON NOMBRE, IMAGEN Y VIVO/MUERTO
-            charactersHtml.innerHTML += 
-            `<li class="card">
-                <img class="card-image" src="${character.img}" alt="${character.name}">
-                <h2 class="card-name">${character.name}</h2>
-                <h2 class="card-life">${character.status}</h2>
-            </li>`;
-        }
+        //LLAMA A LA FUNCIÓN showCaracterCards PASANDO EL data QUE RECIBO DEL API (LINE41)
+        showCharacterCards(data);
     });
 
 
-    
+//EVENTO
+buttonSearchHtml.addEventListener("click", (event) => {
+    event.preventDefault();
+    //LLAMAR A LA API
+    fetch(`https://breakingbadapi.com/api/characters?name=${inputSearchHtml.value}`)
+    .then(function (response) {
+        return response.json();
+    })
+    //INFO QUE DEVUELVE LA API
+    .then(function(data) { 
+        //LLAMA A LA FUNCIÓN showCaracterCards PASANDO EL data QUE RECIBO DEL API (LINE41)
+        showCharacterCards(data);
+    });
+});
+
+
+//FUNCIÓN QUE MUESTRA LAS TARJETAS DE PERSONAJES RECIBIENDO data 
+function showCharacterCards(data) {
+    //LIMPIA EL CONTENIDO (MENSAJE 'CARGANDO...')
+    charactersHtml.innerHTML = '';
+    //GUARDA RESPUESTA DE API EN NUESTRO ARRAY (LISTA DE PERSONAJES)
+    characterList = data;
+    //RECORRE LISTA DE PERSONAJES
+    for (const character of characterList) {
+        //CREA Y MUESTRA TARJETA CON NOMBRE, IMAGEN Y VIVO/MUERTO
+        charactersHtml.innerHTML += 
+        `<li class="card">
+            <img class="card-image" src="${character.img}" alt="${character.name}">
+            <h2 class="card-name">${character.name}</h2>
+            <h2 class="card-life">
+            </h2>
+        </li>`;
+    }
+};
+
+
